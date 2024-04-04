a3440bb304422f985f28a269dc9571f6fd9aeab4
Scott Marinoff <smarinoff@gmail.com>
2024-04-04 12:06:50 -0400

    fix index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ requirejs(['assets/js/mixpanel.amd.js'], function(mixpanel) {
     mixpanel.init("c776e46536c51dc4aac9940cdd098961", {
         debug: true,
         loaded: function() {
-            debug: true, 
-            track_pageview: "url-with-path-and-query-string", 
-            persistence: 'localStorage'
+            debug: true
+            // track_pageview: "url-with-path-and-query-string", 
+            // persistence: 'localStorage'
         }
     });
 });
