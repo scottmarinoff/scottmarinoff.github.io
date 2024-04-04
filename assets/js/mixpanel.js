31f6f3d96672a530c25a162bfa5ee94645a2e7e2
Scott Marinoff <smarinoff@gmail.com>
2024-04-04 11:07:56 -0400

    Moved Mixpanel to .js
    Created file, copied init script there

---
+++ assets/js/mixpanel.js
@@ -0,0 +1,3 @@
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('c776e46536c51dc4aac9940cdd098961', {debug: true, track_pageview: "url-with-path-and-query-string", persistence: 'localStorage'});
