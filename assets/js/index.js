bb65ce949a9f801e902904bc19c4ea7955eeb6fd
Scott Marinoff <smarinoff@gmail.com>
2024-04-04 12:03:00 -0400

    remove properties

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+requirejs(['assets/js/mixpanel.amd.js'], function(mixpanel) {
+
+    mixpanel.init("c776e46536c51dc4aac9940cdd098961", {
+        debug: true,
+        loaded: function() {
+            debug: true
+            // track_pageview: "url-with-path-and-query-string", 
+            // persistence: 'localStorage'
+        }
+    });
+});
